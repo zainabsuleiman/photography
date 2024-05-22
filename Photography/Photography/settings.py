93b347b5ed95ebbd465606b7cb3a892ad7a66503
Nyinawumuntu zainab suleiman <33893536+zainabsuleiman@users.noreply.github.com>
2024-05-22 10:47:17 +0200

    Update settings.py

--- Photography/Photography/settings.py
+++ Photography/Photography/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-m4@17*n86i*1ey(64fip!%=eo81e&u)z@qd8sme)=i)8xn2w13
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
